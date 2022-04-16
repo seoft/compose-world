03212f1155cdc8030c0b2505833e435a43ae232f
seoft <seoft.dev@gmail.com>
2022-04-16 19:31:51 +0900

    ChatRecord 뷰,터치이벤트,콜백 종합구성

--- cw01/app/src/main/java/kr/co/seoft/cw01/WorkSpaceActivity.kt
+++ cw01/app/src/main/java/kr/co/seoft/cw01/WorkSpaceActivity.kt
@@ -3,12 +3,14 @@ package kr.co.seoft.cw01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import kr.co.seoft.cw01.compose.AudioRecordEvent
+import kr.co.seoft.cw01.compose.ChatBottomLayoutContainer
 import kr.co.seoft.cw01.ui.theme.Composeworld01Theme
+import kr.co.seoft.cw01.utils.e
 
 class WorkSpaceActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,16 +22,21 @@ class WorkSpaceActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Box(Modifier.fillMaxSize()) {
-
-// * ChatBottomLayout 채팅 페이지 내 하단 가변 UI 레이아웃
-//                        ChatBottomLayout(
-//                            Modifier.align(Alignment.BottomCenter),
-//                            onImageClick = {},
-//                            onCameraClick = {},
-//                            onSendClick = {},
-//                        )
-                    }
+                    // * ChatBottomLayout 채팅 페이지 내 하단 가변 UI 레이아웃을 포함한 full 레이아웃
+                    ChatBottomLayoutContainer(
+                        Modifier.fillMaxSize(),
+                        onImageClick = {},
+                        onCameraClick = {},
+                        onSendClick = {},
+                        onAudioRecordEvent = {
+                            when (it) {
+                                AudioRecordEvent.START -> "START".e()
+                                AudioRecordEvent.SEND -> "SEND".e()
+                                AudioRecordEvent.CANCEL -> "CANCEL".e()
+                                AudioRecordEvent.TOO_SHORT_TO_FAIL -> "TOO_SHORT_TO_FAIL".e()
+                            }
+                        }
+                    )
                 }
             }
         }
--- cw01/app/src/main/java/kr/co/seoft/cw01/compose/ChatBottomLayout.kt
+++ cw01/app/src/main/java/kr/co/seoft/cw01/compose/ChatBottomLayout.kt
@@ -1,9 +1,14 @@
 package kr.co.seoft.cw01.compose
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -11,8 +16,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEvent
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,19 +29,109 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import kr.co.seoft.cw01.R
 import kr.co.seoft.cw01.ui.theme.Composeworld01Theme
+import java.lang.Integer.max
+import java.lang.Integer.min
+import java.util.*
+import kotlin.math.roundToInt
 
 @Preview(showBackground = true, widthDp = 360, heightDp = 800)
 @Composable
 fun ChatBottomLayoutPreview() {
     Composeworld01Theme {
-        Box(Modifier.fillMaxSize()) {
-            ChatBottomLayout(
-                Modifier.align(Alignment.BottomCenter),
-                onImageClick = {},
-                onCameraClick = {},
-                onSendClick = {}
+        ChatBottomLayoutContainer(
+            modifier = Modifier.fillMaxSize(),
+            onImageClick = {},
+            onCameraClick = {},
+            onSendClick = {},
+            onAudioRecordEvent = {},
+        )
+    }
+}
+
+@Composable
+fun ChatBottomLayoutContainer(
+    modifier: Modifier,
+    onImageClick: () -> Unit,
+    onCameraClick: () -> Unit,
+    onSendClick: () -> Unit,
+    onAudioRecordEvent: (AudioRecordEvent) -> Unit
+) {
+    val recordViewMinBottomMargin = 92
+    val recordViewMaxBottomMargin = 220
+    val minRecordTime = 1500
+
+    var recordLayoutShowState by remember { mutableStateOf(false) }
+    var recordLayoutShowTime by remember { mutableStateOf(0L) }
+    var recordViewBottomMargin by remember { mutableStateOf(recordViewMinBottomMargin) }
+
+    Box(modifier = modifier.fillMaxSize()) {
+        ChatBottomLayout(
+            Modifier.align(Alignment.BottomCenter),
+            onImageClick = onImageClick,
+            onCameraClick = onCameraClick,
+            onSendClick = onSendClick,
+            onAudioEventStatus = {
+                when (it) {
+                    is AudioViewTouchEvent.Start -> {
+                        recordLayoutShowState = true
+                        recordLayoutShowTime = Date().time
+                        recordViewBottomMargin = recordViewMinBottomMargin
+                        onAudioRecordEvent.invoke(AudioRecordEvent.START)
+                    }
+                    is AudioViewTouchEvent.Move -> {
+                        val currentBottomMargin = max(
+                            recordViewMinBottomMargin, recordViewMinBottomMargin + it.positionY
+                        )
+                        recordViewBottomMargin = min(recordViewMaxBottomMargin, currentBottomMargin)
+                    }
+                    is AudioViewTouchEvent.End -> {
+                        if (recordViewBottomMargin == recordViewMaxBottomMargin) {
+                            onAudioRecordEvent.invoke(AudioRecordEvent.CANCEL)
+                        } else {
+                            if ((recordLayoutShowTime + minRecordTime) < Date().time) {
+                                onAudioRecordEvent.invoke(AudioRecordEvent.SEND)
+                            } else {
+                                onAudioRecordEvent.invoke(AudioRecordEvent.TOO_SHORT_TO_FAIL)
+                            }
+                        }
+                        recordLayoutShowState = false
+                        recordLayoutShowTime = -1
+                    }
+                }
+            }
+        )
+        if (recordLayoutShowState) {
+            ChatRecordLayout(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                recordViewBottomMargin = recordViewBottomMargin,
+                recordViewBottomMargin == recordViewMaxBottomMargin
             )
         }
+    }
+}
+
+@Composable
+fun ChatRecordLayout(modifier: Modifier, recordViewBottomMargin: Int, inCancelBoundary: Boolean) {
+    Box(
+        modifier = modifier
+            .height(268.dp)
+            .background(Color(0x33123123))
+    ) {
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(bottom = 220.dp)
+                .size(48.dp)
+                .clip(CircleShape)
+                .background(Color.Gray)
+        )
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = recordViewBottomMargin.dp)
+                .size(136.dp, 54.dp)
+                .background(if (inCancelBoundary) Color.Red else Color.Black)
+        )
     }
 }
 
@@ -42,7 +140,8 @@ fun ChatBottomLayout(
     modifier: Modifier = Modifier,
     onImageClick: () -> Unit,
     onCameraClick: () -> Unit,
-    onSendClick: () -> Unit
+    onSendClick: () -> Unit,
+    onAudioEventStatus: (AudioViewTouchEvent) -> Unit
 ) {
     var textState by remember { mutableStateOf("") }
     var isOptionEnableState by remember { mutableStateOf(true) }
@@ -122,9 +221,13 @@ fun ChatBottomLayout(
             }) {
 
             if (isOptionEnableState) {
-                IconButton(modifier = Modifier
-                    .size(commonIconOuterSize), onClick = { }) {
+                Box(
+                    modifier = Modifier
+                        .size(commonIconOuterSize)
+                        .audioTouchEvent(onAudioEventStatus)
+                ) {
                     Icon(
+                        modifier = Modifier.align(Alignment.Center),
                         painter = painterResource(id = R.drawable.ic_mic),
                         tint = Color.Black,
                         contentDescription = null
@@ -144,6 +247,52 @@ fun ChatBottomLayout(
             }
         }
     }
+}
+
+
+private const val NOT_LONG_TOUCHED_STATUS = -1
+
+// 터치 컨트롤 ref : https://stackoverflow.com/questions/64571945/how-can-i-get-ontouchevent-in-jetpack-compose
+@SuppressLint("UnnecessaryComposedModifier")
+private fun Modifier.audioTouchEvent(
+    onAudioEventStatus: (AudioViewTouchEvent) -> Unit
+): Modifier = composed {
+
+    var firstTouchedPositionY by remember { mutableStateOf(NOT_LONG_TOUCHED_STATUS) }
+
+    this
+        .pointerInput(Unit) {
+            detectTapGestures(
+                onLongPress = {
+                    firstTouchedPositionY = it.y.roundToInt()
+                    onAudioEventStatus.invoke(AudioViewTouchEvent.Start)
+                }
+            )
+        }
+        .pointerInput(Unit) {
+            forEachGesture {
+                awaitPointerEventScope {
+                    awaitFirstDown()
+                    do {
+                        val event: PointerEvent = awaitPointerEvent()
+                        // ACTION_MOVE loop
+                        if (firstTouchedPositionY != NOT_LONG_TOUCHED_STATUS) {
+                            event.changes.getOrNull(0)?.position?.y?.let {
+                                onAudioEventStatus.invoke(
+                                    AudioViewTouchEvent.Move(firstTouchedPositionY - it.roundToInt())
+                                )
+                            }
+                        }
+                    } while (event.changes.any { it.pressed })
+
+                    // ACTION_TOUCH_UP
+                    if (firstTouchedPositionY != NOT_LONG_TOUCHED_STATUS) {
+                        onAudioEventStatus.invoke(AudioViewTouchEvent.End)
+                    }
+                    firstTouchedPositionY = NOT_LONG_TOUCHED_STATUS
+                }
+            }
+        }
 }
 
 @Composable
@@ -187,3 +336,10 @@ fun ChatBottomTextField(
 
 }
 
+sealed class AudioViewTouchEvent {
+    object Start : AudioViewTouchEvent()
+    data class Move(val positionY: Int) : AudioViewTouchEvent()
+    object End : AudioViewTouchEvent()
+}
+
+enum class AudioRecordEvent { START, SEND, CANCEL, TOO_SHORT_TO_FAIL }
